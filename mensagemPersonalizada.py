0176cb77baa2e0493f5f2a577608066459b21765
Alberes Silva <alberessilva@yahoo.com.br>
2024-09-12 11:06:40 -0300

    Create mensagemPersonalizada.py

---
+++ mensagemPersonalizada.py
@@ -0,0 +1,10 @@
+idade = int(input("Digite a sua idade: "))
+
+if 0 <= idade <=12:
+    print('A criança é o amor feito visível.')
+elif   13 <= idade <=17:
+    print('Há em cada adolescente um mundo encoberto, um almirante e um sol de outubro.')
+elif 18 <= idade <= 64:
+    print('O jovem tem todos os defeitos do adulto e mais um: o da inexperiência.')
+else:
+    print('O idoso é uma biblioteca viva.')
