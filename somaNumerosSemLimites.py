239c1e8b34bd819ed87c9ae7f704a27d15439965
Alberes Silva <alberessilva@yahoo.com.br>
2024-09-15 21:09:59 -0300

    Create somaNumerosSemLimites.py

---
+++ somaNumerosSemLimites.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um número (inteiro): '))
+resultado= 0
+while numero >= 1:
+    resultado += numero
+    numero -= 1
+print(f'A soma de todos os números é {resultado}')
+    
