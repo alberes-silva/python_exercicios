0d7036799cb26f7438af1b89b77bc008202abc86
Alberes Silva <alberessilva@yahoo.com.br>
2024-09-16 09:25:42 -0300

    Create fatorialNumeros.py

---
+++ fatorialNumeros.py
@@ -0,0 +1,21 @@
+from math import factorial
+
+#Fatorial usando a biblioteca math
+# numero = int(input('Digite um número: '))
+# fatorialNumero = factorial(numero)
+# print(f'O fatorial de {numero} é {fatorialNumero}')
+
+
+#Fatorial tradicional
+numero = int(input('Digite um número: '))
+if numero < 0:
+    print('Por favor, digite um número inteiro positivo')
+else:
+    fatorial = 1
+    for i in range(1, numero +1):
+        fatorial *= i
+    print(f'O fatorial de {numero} é igual {fatorial}')
+
+
+
+
