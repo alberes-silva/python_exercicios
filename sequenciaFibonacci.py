d1a74525e8debb174ececa2a0bd7cfc3705a40e4
Alberes Silva <alberessilva@yahoo.com.br>
2024-09-18 08:03:10 -0300

    Create sequenciaFibonacci.py

---
+++ sequenciaFibonacci.py
@@ -0,0 +1,14 @@
+numero = int(input('Digite o valor da sequência desejada: '))
+
+contador = 3
+priSeq = 0
+segSeq = 1
+
+
+print(f'{priSeq} -> {segSeq}', end='')
+while contador <= numero:
+    tercSeq = priSeq + segSeq
+    print(f' -> {tercSeq}', end='')
+    priSeq = segSeq
+    segSeq = tercSeq
+    contador +=1
