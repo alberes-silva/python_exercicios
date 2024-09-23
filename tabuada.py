83f5c986492203f059243ae0c55c1caa044e7572
Alberes Silva <alberessilva@yahoo.com.br>
2024-09-23 15:51:06 -0300

    Create tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,8 @@
+for numero in range(1, 11):
+    print("Tabuada de {}:".format(numero))
+
+    for mult in range(1, 11):
+        valor = numero * mult
+        print(f"{numero} x {mult} = {valor}")
+
+    print()
